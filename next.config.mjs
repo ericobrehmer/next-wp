688f45d1ee023745ad3a752f1e766c6ce5d6f6c8
Erico <ericobrehmer@gmail.com>
2024-10-22 16:16:16 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.WORDPRESS_HOSTNAME,
+        hostname: "dev.adon-line.de",
         port: "",
         pathname: "/**",
       },
