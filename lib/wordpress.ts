f0f71dbeadcc654fc983740536597087566820ee
Erico Brehmer <ericobrehmer@adonimac4.localdomain>
2024-10-23 12:13:56 +0200

    testing env variable

--- lib/wordpress.ts
+++ lib/wordpress.ts
@@ -15,11 +15,12 @@ import {
 
 // WordPress Config
 
-const baseUrl = process.env.WORDPRESS_URL;
+//const baseUrl = process.env.WORDPRESS_URL;
+const baseUrl = "https://dev.adon-line.de";
 
 function getUrl(path: string, query?: Record<string, any>) {
     const params = query ? querystring.stringify(query) : null
-  
+
     return `${baseUrl}${path}${params ? `?${params}` : ""}`
 }
 
@@ -29,7 +30,7 @@ export async function getAllPosts(filterParams?: {
   author?: string;
   tag?: string;
   category?: string;
-}): Promise<Post[]> {  
+}): Promise<Post[]> {
   const url = getUrl("/wp-json/wp/v2/posts", { author: filterParams?.author, tags: filterParams?.tag, categories: filterParams?.category });
   const response = await fetch(url);
   const posts: Post[] = await response.json();
